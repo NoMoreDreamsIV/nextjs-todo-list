a646972840a366c27cd4a40af569ace4f93cdf87
NoMoreDreamsIV <kronkdubs@gmail.com>
2023-03-03 14:39:20 +1000

    to do list

---
+++ components/todoform.js
@@ -0,0 +1,26 @@
+export function TodoForm({ addTask, deleteMarked }) {
+    const [userInput, setUserInput] = React.useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addTask(userInput);
+        setUserInput('');
+    }
+
+    const handleChange = (event) => {
+        setUserInput(event.currentTarget.value);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                value={userInput}
+                type="text"
+                onChange={handleChange}
+                placeholder='Что добавить?'
+            />
+            <button>Добавить</button>
+            <button onClick={deleteMarked}>Удалить отмеченное</button>
+        </form>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import { TodoForm } from './todoform.js'
+import { Todo } from './todo.js'
+
+export default function App() {
+  const [todos, setTodos] = React.useState([])
+  const addTask = (userInput) => {
+      if (userInput) {
+          const newTask = {
+              id: Date.now(),
+              task: userInput,
+              complete: false
+          }
+          setTodos([...todos, newTask]);
+      }
+  }
+
+  const removeTask = (id) => {
+      setTodos([...todos.filter((todo) => todo.id !== id)])
+  }
+
+  const handleToggle = (id) => {
+      setTodos([...todos.map((todo) => todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo })])
+  }
+
+  const deleteMarked = () => {
+      setTodos([...todos.filter((todo) => !todo.complete)])
+  }
+
+  return (
+      <div className='App'>
+          <header>
+              <h1>Список задач: {todos.length}</h1>
+          </header>
+          <TodoForm
+              addTask={addTask}
+              deleteMarked={deleteMarked}
+          />
+          {todos.map((todo) => {
+              return (
+                  <Todo
+                      todo={todo}
+                      key={todo.id}
+                      toggleTask={handleToggle}
+                      removeTask={removeTask}
+                  />
+              )
+          })}
+      </div>
+  );
+}
