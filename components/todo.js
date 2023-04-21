cb5184d5893c74bbf44e50facfebe42397c37197
NoMoreDreamsIV <kronkdubs@gmail.com>
2023-04-22 00:58:39 +1000

    added drag'n'drop

--- components/todo.js
+++ components/todo.js
@@ -1,7 +1,13 @@
-export function Todo({ todo, toggleTask, removeTask }) {
+export function Todo({ todo, toggleTask, removeTask, dragStartHandle, dragEndHandle, dragOverHandle, dropHandle }) {
     return (
         <div key={todo.id} className='item-todo'>
             <div
+                onDragStart={(event) => dragStartHandle(event, todo)}
+                onDragLeave={(event) => dragEndHandle(event)}
+                onDragEnd={(event) => dragEndHandle(event)}
+                onDragOver={(event) => dragOverHandle(event)}
+                onDrop={(event) => dropHandle(event, todo)}
+                draggable={true}
                 className={todo.complete ? 'item-text strike' : 'item-text'}
                 onClick={() => toggleTask(todo.id)}
             >
--- pages/index.js
+++ pages/index.js
@@ -3,49 +3,89 @@ import { Todo } from '/components/todo.js'
 import { useState } from 'react'
 
 export default function App() {
-  const [todos, setTodos] = useState([])
-  const addTask = (userInput) => {
-      if (userInput) {
-          const newTask = {
-              id: Date.now(),
-              task: userInput,
-              complete: false
-          }
-          setTodos([...todos, newTask]);
-      }
-  }
+    const [todos, setTodos] = useState([]);
+    const [currentItem, setCurrentItem] = useState(null);
+    const addTask = (userInput) => { 
+        if (userInput) {
+            const newTask = {
+                id: Date.now(),
+                task: userInput,
+                complete: false
+            }
+            setTodos([...todos, newTask]);
+        }
+    }
 
-  const removeTask = (id) => {
-      setTodos([...todos.filter((todo) => todo.id !== id)])
-  }
+    const removeTask = (id) => {
+        setTodos([...todos.filter((todo) => todo.id !== id)])
+    }
 
-  const handleToggle = (id) => {
-      setTodos([...todos.map((todo) => todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo })])
-  }
+    const handleToggle = (id) => {
+        setTodos([...todos.map((todo) => todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo })])
+    }
 
-  const deleteMarked = () => {
-      setTodos([...todos.filter((todo) => !todo.complete)])
-  }
+    const deleteMarked = () => {
+        setTodos([...todos.filter((todo) => !todo.complete)])
+    }
 
-  return (
-      <div className='App'>
-          <header>
-              <h1>Список задач: {todos.length}</h1>
-          </header>
-          <TodoForm
-              addTask={addTask}
-              deleteMarked={deleteMarked}
-          />
-          {todos.map((todo) => {
-              return (
-                  <Todo
-                      todo={todo}
-                      key={todo.id}
-                      toggleTask={handleToggle}
-                      removeTask={removeTask}
-                  />
-              )
-          })}
-      </div>
-  );
+    const dragStartHandle = (event, todo) => {
+        setCurrentItem(todo);
+    }
+
+    const dragEndHandle = (event) => {
+        event.target.style.background = 'rgb(0, 158, 150)';
+    }
+
+    const dragOverHandle = (event) => {
+        event.preventDefault();
+        event.target.style.background = 'rgb(0, 77, 73)';
+    }   
+
+    const dropHandle = (event, todo) => {
+        event.preventDefault();
+        setTodos(todos.map(item => {
+            if (item.id === todo.id) {
+                return {...item, id: currentItem.id}
+            }
+            if (item.id === currentItem.id) {
+                return {...item, id: todo.id}
+            }
+            return item;
+        }))
+        event.target.style.background = 'rgb(0, 158, 150)';
+    }
+
+    const sortTodos = (a, b) => {
+        if (a.id > b.id) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    return (
+        <div className='App'>
+            <header>
+                <h1>Список задач: {todos.length}</h1>
+            </header>
+            <TodoForm
+                addTask={addTask}
+                deleteMarked={deleteMarked}
+            />
+            {todos.sort(sortTodos).map((todo) => {
+                return (
+                    <Todo
+                        todo={todo}
+                        key={todo.id}
+                        toggleTask={handleToggle}
+                        removeTask={removeTask}
+                        dragStartHandle={dragStartHandle}
+                        dragEndHandle={dragEndHandle}
+                        dragOverHandle={dragOverHandle}
+                        dropHandle={dropHandle}
+                    />
+                )
+            })}
+        </div>
+    );
 }
